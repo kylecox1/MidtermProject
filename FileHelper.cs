b846a786bb9f9d350bb94a719dfd71d196146980
Em Ma <emilymtsn3@gmail.com>
2021-11-29 21:01:21 -0600

    Added Book List

--- FileHelper.cs
+++ FileHelper.cs
@@ -10,6 +10,18 @@ namespace Library
         public static List<Book> BookList()
         {
             List<Book> books = new List<Book>();
+            books.Add(new Book("Child Theif","Christopher Brom",Genre.Horror));
+            books.Add(new Book("Lightning Theif","Rick Riordan",Genre.Fantasy));
+            books.Add(new Book("Chains", "Laurie Halse Anderson", Genre.HistoricalFiction));
+            books.Add(new Book("Cinder", "Marissa Meyer", Genre.SciFi));
+            books.Add(new Book("Sherlock Holmes", "Arthur Conan Doyle", Genre.Mystery));
+            books.Add(new Book("Buried Onions", "Gary Soto", Genre.RealisticFiction));
+            books.Add(new Book("Fabio", "Ya Dream Man", Genre.Romance));
+            books.Add(new Book("Coding for Dummies", "NikHil Abraham", Genre.NonFiction));
+            books.Add(new Book("Green Eggs and Ham", "Dr Seuss", Genre.ChildrensBooks));
+            books.Add(new Book("Foundation", "Isaac Asimo", Genre.SciFi));
+            books.Add(new Book("Hyperion", "Dan Simmons", Genre.SciFi));
+            books.Add(new Book("Cockoo Song", "Frances Hardinge", Genre.Horror));
             return books;
         }
         public static void WriteToFile()
