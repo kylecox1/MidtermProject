b677a6f36f849d97fa09d33358029157dc3253c4
kylecox1 <33430803+kylecox1@users.noreply.github.com>
2021-12-02 21:32:31 -0500

    UI methods into helpermethods, author search and checkout implemented

--- FileHelper.cs
+++ FileHelper.cs
@@ -9,29 +9,29 @@ namespace Library
     public class FileHelper
     {
 
-        //public static List<Book> BookList()
-        //{
-        //    List<Book> books = new List<Book>();
-        //    books.Add(new Book("Child Theif", "Christopher Brom", Genre.Horror));
-        //    books.Add(new Book("Lightning Theif", "Rick Riordan", Genre.Fantasy));
-        //    books.Add(new Book("Chains", "Laurie Halse Anderson", Genre.HistoricalFiction));
-        //    books.Add(new Book("Cinder", "Marissa Meyer", Genre.SciFi));
-        //    books.Add(new Book("Sherlock Holmes", "Arthur Conan Doyle", Genre.Mystery));
-        //    books.Add(new Book("Buried Onions", "Gary Soto", Genre.RealisticFiction));
-        //    books.Add(new Book("Fabio", "Ya Dream Man", Genre.Romance));
-        //    books.Add(new Book("Coding for Dummies", "NikHil Abraham", Genre.NonFiction));
-        //    books.Add(new Book("Green Eggs and Ham", "Dr Seuss", Genre.ChildrensBooks));
-        //    books.Add(new Book("Foundation", "Isaac Asimo", Genre.SciFi));
-        //    books.Add(new Book("Hyperion", "Dan Simmons", Genre.SciFi));
-        //    books.Add(new Book("Cockoo Song", "Frances Hardinge", Genre.Horror));
-        //    return books;
-        //}
+        public static List<Book> BookList()
+        {
+            List<Book> books = new List<Book>();
+            books.Add(new Book("Child Theif", "Christopher Brom", Genre.Horror));
+            books.Add(new Book("Lightning Theif", "Rick Riordan", Genre.Fantasy));
+            books.Add(new Book("Chains", "Laurie Halse Anderson", Genre.HistoricalFiction));
+            books.Add(new Book("Cinder", "Marissa Meyer", Genre.SciFi));
+            books.Add(new Book("Sherlock Holmes", "Arthur Conan Doyle", Genre.Mystery));
+            books.Add(new Book("Buried Onions", "Gary Soto", Genre.RealisticFiction));
+            books.Add(new Book("Fabio", "Ya Dream Man", Genre.Romance));
+            books.Add(new Book("Coding for Dummies", "NikHil Abraham", Genre.NonFiction));
+            books.Add(new Book("Green Eggs and Ham", "Dr Seuss", Genre.ChildrensBooks));
+            books.Add(new Book("Foundation", "Isaac Asimov", Genre.SciFi));
+            books.Add(new Book("Hyperion", "Dan Simmons", Genre.SciFi));
+            books.Add(new Book("Cockoo Song", "Frances Hardinge", Genre.Horror));
+            return books;
+        }
         public static List<Book> GetFileBookList()
         {
             StreamReader reader = new StreamReader(@"c:\code\books.txt");
             List<Book> books = new List<Book>();
             string line;
-            while((line = reader.ReadLine()) != null)
+            while ((line = reader.ReadLine()) != null)
             {
                 string[] bookData = line.Split('|');
                 Book newBook = new Book(bookData[0], bookData[1], Enum.Parse<Genre>(bookData[2]),
--- HelperMethods.cs
+++ HelperMethods.cs
@@ -1,11 +1,182 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
+using System.Linq;
 
 namespace Library
 {
     public class HelperMethods
     {
-        //for making things go smoothly
+        public static void UserInterface()
+        {
+            Console.ForegroundColor = ConsoleColor.DarkCyan;
+            List<Book> books = FileHelper.GetFileBookList();
+
+            Console.WriteLine("Welcome to the world's smallest library!");
+            Console.WriteLine("");
+            Console.WriteLine("These are our current selections:");
+            Console.WriteLine("");
+
+            foreach (var book in books)
+            {
+                Console.WriteLine($"  --- \"{ book.Title }\" by { book.Author } ---");
+            }
+            Console.WriteLine("");
+            bool continueBrowsing = true;
+            while (continueBrowsing == true)
+            {
+                Console.WriteLine("To continue, please enter:");
+                Console.WriteLine("1. to search by author.");
+                Console.WriteLine("2. to search by title.");
+                Console.WriteLine("3. to return books.");
+                Console.WriteLine("4. to donate books.");
+                Console.WriteLine("Q to exit.");
+                bool validSelection = false;
+                while (validSelection == false)
+                {
+
+                    string userSearchSelection = Console.ReadLine();
+                    if (userSearchSelection == "1")
+                    {
+                        validSelection = true;
+                        bool continueAuthorSearch = true;
+                        int userAuthorSelection;
+                        while (continueAuthorSearch == true)
+                        {
+                            List<Book> authorSearchResults = AuthorSearch(books);
+                            if (authorSearchResults.Count == 0)
+                            {
+                                Console.WriteLine("Sorry, we did not find any books with that author.");
+                                Console.Write("Search by author again? (y/n): ");
+                                continueAuthorSearch = GetYesOrNo();
+                                continue;
+                            }
+                            Console.WriteLine("For that search, we have the following book(s):");
+                            Console.WriteLine("");
+                            int count = 1;
+                            foreach (var book in authorSearchResults)
+                            {
+                                Console.WriteLine($"{ count }. \"{ book.Title }\" by { book.Author }");
+                                count++;
+                            }
+                            Console.WriteLine("");
+                            bool checkOutFromAuthorSearch = false;
+                            Console.Write("Would you like to check out any of these? (y/n): ");
+                            checkOutFromAuthorSearch = GetYesOrNo();
+                            if (checkOutFromAuthorSearch == false)
+                            {
+                                Console.Write("Search by author again? (y/n): ");
+                                continueAuthorSearch = GetYesOrNo();
+                            }
+                            else if (checkOutFromAuthorSearch == true)
+                            {
+                                bool validInput = false;
+                                while (validInput == false)
+                                {
+                                    Console.Write("Please type the number of the book you would like: ");
+                                    string userInput = Console.ReadLine();
+                                    validInput = int.TryParse(userInput, out userAuthorSelection);
+                                    if (validInput)
+                                    {
+                                        if (userAuthorSelection > authorSearchResults.Count || userAuthorSelection < 1)
+                                        {
+                                            Console.WriteLine("Sorry, that number was out of range.");
+                                            validInput = false;
+                                        }
+                                        else
+                                        {
+                                            Book bookToCheckOut = authorSearchResults[userAuthorSelection - 1];
+                                            Console.WriteLine($"Book number { userAuthorSelection } is " +
+                                                $"\"{ bookToCheckOut.Title }\" by { bookToCheckOut.Author }");
+                                            if (IsDue.CheckOutBook(bookToCheckOut))
+                                            {
+                                                bookToCheckOut.BookStatus = true;
+                                                bookToCheckOut.DueDate = IsDue.DueDate();
+                                                Console.WriteLine($"Looks like it was available! Please return it by { bookToCheckOut.DueDate }");
+                                            }
+                                        }
+                                        continueAuthorSearch = false;
+                                    }
+                                    else
+                                    {
+                                        Console.WriteLine("Please enter a whole number only.");
+                                    }
+                                }
+                            }
+                        }
+                    }
+
+                    else if (userSearchSelection == "2")
+                    {
+                        validSelection = true;
+                        TitleSearch(books);
+
+                    }
+                    else if (userSearchSelection == "3")
+                    {
+                        validSelection = true;
+                        Console.WriteLine("Logic to accept returns.");
+
+                    }
+                    else if (userSearchSelection == "4")
+                    {
+                        validSelection = true;
+                        Console.WriteLine("Logic to accept donations.");
+                    }
+                    else if (userSearchSelection.ToLower() == "q")
+                    {
+                        validSelection = true;
+                        continueBrowsing = false;
+                    }
+                    else
+                    {
+                        Console.Write("Sorry, that number was out of range. Try again please: ");
+                    }
+                }
+                if (continueBrowsing == true)
+                {
+                    Console.Write("Continue browsing? (y/n): ");
+                    continueBrowsing = GetYesOrNo();
+                }
+            }
+            Console.WriteLine("Thanks for visiting us today! Press any key to exit.");
+            Console.ReadKey();
+        }
+
+        public static List<Book> AuthorSearch(List<Book> books)
+        {
+            Console.Write("Please enter the author search term: ");
+            string userAuthorSearch = Console.ReadLine();
+            List<Book> bookQuery = books.Where(p => p.Author.ToLower().Contains(userAuthorSearch.ToLower())).ToList();
+            return bookQuery;
+        }
+
+        public static void TitleSearch(List<Book> books)
+        {
+            Console.Write("Please enter the title search term: ");
+            string userTitleSearch = Console.ReadLine();
+            List<Book> bookQuery = books.Where(p => p.Title.ToLower().Contains(userTitleSearch.ToLower())).ToList();
+        }
+
+        public static bool GetYesOrNo()
+        {
+            bool isValidYesNo = false;
+            do
+            {
+                string input = Console.ReadLine();
+                if (input.Trim().ToLower() == "n")
+                {
+                    return false;
+                }
+                else if (input.Trim().ToLower() == "y")
+                {
+                    return true;
+                }
+                else
+                {
+                    Console.Write("Please just enter a \"y\" or an \"n\": ");
+                }
+            } while (isValidYesNo == false);
+            return false;
+        }
     }
 }
